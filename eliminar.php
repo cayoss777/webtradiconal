e923cc16a04757ff6e1d4a52fd5b685910d5a608
cayoss777 <cayoss.77@gmail.com>
2024-06-12 12:30:54 -0500

    Adicionar datos dos

---
+++ eliminar.php
@@ -0,0 +1,8 @@
+<?php
+include 'conecion.php';
+if (isset($_GET['eli_id'])) {
+ $eli_sql="delete from vendedor where id='$_GET[eli_id]'";
+ $con_sql=mysqli_query($coneccion,$eli_sql);
+}
+include 'mostrar.php';
+?>
---
+++ mostrar.php
@@ -0,0 +1,31 @@
+
+<?php
+include 'conecion.php';
+?>
+<table class="table table-striped">
+   <thead>
+    <th> Id</th>
+    <th> Vendedor</th>
+    <th> Dirección</th>
+    <th> Fecha Venta</th>
+    <th> Acción</th>
+   </thead>
+   <tbody>
+    <?php
+     $sql="select * from vendedor";
+     $con_sql=mysqli_query($coneccion,$sql);
+      while ($rows=mysqli_fetch_array($con_sql)) {
+       echo '
+         <tr>
+          <td>'.$rows['id'].'</td>
+          <td>'.$rows['vendedor'].'</td>
+          <td>'.$rows['direccion'].'</td>
+          <td>'.$rows['fechaventa'].'</td>
+          <td><a class="btn btn-danger btn-xs" href="eliminar.php?eli_id='.$rows['id'].'">Eliminar</td>
+          <td><a class="btn btn-info btn-xs"  href="conexion_detalle.php?det_id='.$rows['id'].'">Detalle</td>
+         </tr>
+       ';
+      }
+    ?>
+   </tbody>
+  </table>
